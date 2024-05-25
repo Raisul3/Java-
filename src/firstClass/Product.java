f427cc6e1648a1969cd40defc4716b92bafa24a3
Raisul3 <ranamdrasel408@gmail.com>
2024-05-25 17:02:29 +0600

    First Commit
    Signed-off-by: Raisul3 <ranamdrasel408@gmail.com>

---
+++ src/firstClass/Product.java
@@ -0,0 +1,11 @@
+package firstClass;
+
+public class Product {
+    public static void main(String[] args) {
+        System.out.println(" Id: 101");
+        System.out.println(" Title: Iphone15");
+        System.out.println(" Price: 1895 Euros");
+        System.out.println(" Description: Perfect product with best image quality");
+        System.out.println(" Category: Phone");
+    }
+}
